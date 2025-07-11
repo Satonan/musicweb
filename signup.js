15a80c1b41498e9e49de6b70bd5d3c9c8c0c96b4
Dang <hn932276@gmail.com>
2025-07-11 16:07:27 +0700

    add library page

---
+++ signup.js
@@ -0,0 +1,48 @@
+let form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let username = document.getElementById("username").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let password = document.getElementById("password").value.trim();
+  let repeatedPassword = document.getElementById("repeatpassword").value.trim();
+
+  let lowerCaseLetter = /[a-z]/g;
+  let upperCaseLetter = /[A-Z]/g;
+  let numbers = /[1-9]/g;
+
+  if (username.length < 6) {
+    alert("username must be at least 6 characters");
+  } else if (password.length < 8) {
+    alert("password must be at least 8 characters");
+  } else if (!password.match(lowerCaseLetter)) {
+    alert("Password must  contain a lowercase letter");
+  } else if (!password.match(upperCaseLetter)) {
+    alert("Password must  contain a uppercase letter");
+  } else if (!password.match(numbers)) {
+    alert("Password must  contain a number or special character");
+  }
+
+  if (localStorage.getItem("users")) {
+    let users = JSON.parse(localStorage.getItem("users"));
+
+    users.push({
+      email,
+      password,
+      username,
+    });
+
+    localStorage.setItem("users", JSON.stringify(users));
+  } else {
+    localStorage.setItem(
+      "users",
+      JSON.stringify([
+        {
+          email,
+          password,
+          username,
+        },
+      ])
+    );
+  }
+});
