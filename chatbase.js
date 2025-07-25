1ef1d89573ded16254dc0cfb8c78eb16079f1b2f
Dang <hn932276@gmail.com>
2025-07-25 15:45:16 +0700

    chat bot

---
+++ chatbase.js
@@ -0,0 +1,8 @@
+// const crypto = require("crypto");
+
+// const secret = ""; // Your verification secret key
+// const userId = current_user.id; // A string UUID to identify your user
+
+// const hash = crypto.createHmac("sha256", secret).update(userId).digest("hex");
+// const apiKey = process.env.API_KEY;
+// console.log(`API Key: ${apiKey}`);
